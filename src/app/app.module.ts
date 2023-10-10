e6db72f1256a231998ebc4f9b6efb1ff213afc18
Dmitriy Mishchenko <ripatrip@gmail.com>
2023-10-10 21:45:45 +0300

    add pwa to cache file requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { EdidListModule } from './edid-list/edid-list.module';
 import { EDID_REPOSITORY_TOKEN } from './base/tokens';
 import { EdidRepositoryService } from './services/edid-repository.service';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, EdidListModule],
+  imports: [BrowserModule, EdidListModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: !isDevMode(),
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [
     { provide: EDID_REPOSITORY_TOKEN, useClass: EdidRepositoryService },
   ],
